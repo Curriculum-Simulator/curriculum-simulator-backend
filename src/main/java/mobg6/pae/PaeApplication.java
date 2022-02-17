e769b8020251ba11da0f042b8e1c32b056717892
ayoub lahyaoui <ayoub.lahyaoui@hotmail.com>
2022-02-17 14:54:35 +0100

    Create Java Spring project

---
+++ src/main/java/mobg6/pae/PaeApplication.java
@@ -0,0 +1,13 @@
+package mobg6.pae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mobg6/pae/PaeApplicationTests.java
@@ -0,0 +1,13 @@
+package mobg6.pae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
