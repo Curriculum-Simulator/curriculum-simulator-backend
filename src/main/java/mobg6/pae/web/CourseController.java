2cc8b0a5d867ea5fb6608d5781bcf8d4c1b944a3
ayoub lahyaoui <ayoub.lahyaoui@hotmail.com>
2022-02-18 11:33:26 +0100

    Add CourseController Controller

---
+++ src/main/java/mobg6/pae/web/CourseController.java
@@ -0,0 +1,13 @@
+package mobg6.pae.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CourseController {
+
+    @GetMapping("/course")
+    public String index() {
+        return "course";
+    }
+}
