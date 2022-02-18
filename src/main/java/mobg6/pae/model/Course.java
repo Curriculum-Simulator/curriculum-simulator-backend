edc309cfa4454c6044960bcac9795e26aca5a9bb
ayoub lahyaoui <ayoub.lahyaoui@hotmail.com>
2022-02-18 11:46:09 +0100

    Update Course model class

--- src/main/java/mobg6/pae/model/Course.java
+++ src/main/java/mobg6/pae/model/Course.java
@@ -1,7 +1,10 @@
 package mobg6.pae.model;
 
+import java.util.List;
+
 public class Course {
     private String id;
     private String title;
     private int credits;
+    private List<Student> students;
 }
