4b97d547b7a9f5b4530a0cfe99cdaea25728b1ce
ayoub lahyaoui <ayoub.lahyaoui@hotmail.com>
2022-02-19 11:50:03 +0100

    Update models

--- src/main/java/mobg6/pae/models/Course.java
+++ src/main/java/mobg6/pae/models/Course.java
@@ -5,8 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Course {
     @Id
--- src/main/java/mobg6/pae/models/Student.java
+++ src/main/java/mobg6/pae/models/Student.java
@@ -4,11 +4,19 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
     @Id
-    private String id;
+    private int id;
     private String name;
+    private String fname;
     private Gender gender;
     private Section section;
     private List<Course> courses;
