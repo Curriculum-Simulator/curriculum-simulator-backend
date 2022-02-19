8c3e2964c7344ac885dbbb16ffa38f66ba348266
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-02-19 14:53:50 +0100

    Fix course seeder

--- src/main/java/mobg6/pae/seeders/CourseSeeder.java
+++ src/main/java/mobg6/pae/seeders/CourseSeeder.java
@@ -1,26 +1,37 @@
 package mobg6.pae.seeders;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStreamReader;
 
-import org.springframework.context.event.EventListener;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
 import mobg6.pae.dao.CourseRepository;
 import mobg6.pae.models.Course;
 import mobg6.pae.models.Section;
 
-public class CourseSeeder {
+@Component
+@RequiredArgsConstructor
+public class CourseSeeder implements CommandLineRunner {
 
     private static final String COMMA_DELIMITER = ",";
 
-    private CourseRepository courseRepository;
+    @Autowired
+    private final CourseRepository courseRepository;
 
+    @Override
+    public void run(String... args) throws Exception {
+        seed();
+    }
 
-    @EventListener
-    public void seed() throws FileNotFoundException, IOException {
+    private void seed() throws CsvValidationException, NumberFormatException, FileNotFoundException, IOException {
         seedSection(Section.COMMUN);
         seedSection(Section.GESTION);
         seedSection(Section.INDUSTRIELLE);
@@ -28,19 +39,21 @@ public class CourseSeeder {
         seedSection(Section.INDUSTRIELLE_RESEAU);
     }
 
-    private void seedSection(Section section) throws FileNotFoundException, IOException {
+    private void seedSection(Section section)
+            throws FileNotFoundException, IOException, CsvValidationException, NumberFormatException {
+        String path = "static/data/cours_" + section.toString().toLowerCase() + ".csv";
+        var fileReader = new InputStreamReader(new ClassPathResource(path).getInputStream());
 
-        try (BufferedReader br = new BufferedReader(new FileReader("book.csv"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(COMMA_DELIMITER);
+        try (CSVReader reader = new CSVReader(fileReader)) {
+            String[] values;
+            while ((values = reader.readNext()) != null) {
                 Course course = new Course();
-                course.setId(values[1]); 
-                course.setTitle(values[2]); 
+                course.setId(values[1]);
+                course.setTitle(values[2]);
                 course.setCredits(Integer.parseInt(values[3]));
                 course.setSection(section);
                 course.setHours(Integer.parseInt(values[4]));
-                course.setStudents(new ArrayList<>()); 
+                // course.setStudents(new ArrayList<>());
                 courseRepository.save(course);
             }
         }
