94c2c1fa5bce7aa884efcba6727e04d367e86fdb
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-02-19 14:54:05 +0100

    Fix student seeder

--- src/main/java/mobg6/pae/seeders/StudentSeeder.java
+++ src/main/java/mobg6/pae/seeders/StudentSeeder.java
@@ -1,22 +1,28 @@
 package mobg6.pae.seeders;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
-import org.springframework.context.event.EventListener;
-
+import lombok.RequiredArgsConstructor;
 import mobg6.pae.dao.StudentRepository;
 import mobg6.pae.models.Gender;
 import mobg6.pae.models.Section;
 import mobg6.pae.models.Student;
 
-public class StudentSeeder {
+@Component
+@RequiredArgsConstructor
+public class StudentSeeder implements CommandLineRunner {
 
-    private StudentRepository studentRepository;
+    @Autowired
+    private final StudentRepository studentRepository;
 
-    @EventListener
-    public void seed() throws FileNotFoundException, IOException {
+    @Override
+    public void run(String... args) throws Exception {
+        seed();
+    }
+
+    private void seed() {
         seedStudent1();
         seedStudent2();
         seedStudent3();
@@ -24,59 +30,59 @@ public class StudentSeeder {
         seedStudent5();
     }
 
-    private void seedStudent1(){
+    private void seedStudent1() {
         Student student = new Student();
         student.setId(54895);
         student.setName("Lahyaoui");
         student.setFname("Ayoub");
         student.setGender(Gender.MALE);
         student.setSection(Section.GESTION);
-        student.setCourses(new ArrayList<>());
+        // student.setCourses(new ArrayList<>());
         studentRepository.save(student);
     }
 
-    private void seedStudent2(){
+    private void seedStudent2() {
         Student student = new Student();
         student.setId(54018);
         student.setName("Routier");
         student.setFname("Basile");
         student.setGender(Gender.MALE);
         student.setSection(Section.GESTION);
-        student.setCourses(new ArrayList<>());
+        // student.setCourses(new ArrayList<>());
         studentRepository.save(student);
     }
 
-    private void seedStudent3(){
+    private void seedStudent3() {
         Student student = new Student();
         student.setId(54356);
         student.setName("Kanan");
         student.setFname("Hosam");
         student.setGender(Gender.MALE);
         student.setSection(Section.GESTION);
-        student.setCourses(new ArrayList<>());
+        // student.setCourses(new ArrayList<>());
         studentRepository.save(student);
     }
 
-    private void seedStudent4(){
+    private void seedStudent4() {
         Student student = new Student();
         student.setId(54247);
         student.setName("Bendaimi");
         student.setFname("Zakaria");
         student.setGender(Gender.MALE);
         student.setSection(Section.GESTION);
-        student.setCourses(new ArrayList<>());
+        // student.setCourses(new ArrayList<>());
         studentRepository.save(student);
     }
 
-    private void seedStudent5(){
+    private void seedStudent5() {
         Student student = new Student();
         student.setId(54985);
         student.setName("Bigham");
         student.setFname("Amine-Ayoub");
         student.setGender(Gender.MALE);
         student.setSection(Section.GESTION);
-        student.setCourses(new ArrayList<>());
+        // student.setCourses(new ArrayList<>());
         studentRepository.save(student);
     }
-    
+
 }
