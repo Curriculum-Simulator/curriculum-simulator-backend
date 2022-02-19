fa00f5ed93ccd1ca399becdb97d20d6490a8543d
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-02-19 12:15:27 +0100

    Add beans and filter fonction

--- src/main/java/mobg6/pae/dao/CourseRepository.java
+++ src/main/java/mobg6/pae/dao/CourseRepository.java
@@ -1,8 +1,15 @@
 package mobg6.pae.dao;
 
-import org.springframework.data.repository.CrudRepository;
-import mobg6.pae.models.Course;
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import mobg6.pae.models.Course;
+import mobg6.pae.models.Section;
+
+@Repository
 public interface CourseRepository extends CrudRepository<Course, String> {
 
+    List<Course> findBySection(Section section);
 }
--- src/main/java/mobg6/pae/dao/StudentRepository.java
+++ src/main/java/mobg6/pae/dao/StudentRepository.java
@@ -1,8 +1,11 @@
 package mobg6.pae.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import mobg6.pae.models.Student;
 
+@Repository
 public interface StudentRepository extends CrudRepository<Student, Integer> {
 
 }
