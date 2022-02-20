e33889250af90ab68de83fc415b221eb561e040d
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-02-20 22:56:21 +0100

    Update index function

--- src/main/java/mobg6/pae/controllers/StudentController.java
+++ src/main/java/mobg6/pae/controllers/StudentController.java
@@ -1,13 +1,24 @@
 package mobg6.pae.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.RequiredArgsConstructor;
+import mobg6.pae.dao.StudentRepository;
+
 @Controller
+@RequiredArgsConstructor
 public class StudentController {
+
+    @Autowired
+    private final StudentRepository studentRepository;
     
     @GetMapping("/student")
-    public String index() {
+    public String index(Model model) {
+        var students = studentRepository.findAll();
+        model.addAttribute("students", students);
         return "student";
     }
 }
