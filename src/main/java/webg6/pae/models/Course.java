500fd89d847909f3fc43d35d55f922852d5a6f50
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-02-23 19:47:44 +0100

    Add validation beans to models

--- src/main/java/webg6/pae/models/Course.java
+++ src/main/java/webg6/pae/models/Course.java
@@ -3,6 +3,8 @@ package webg6.pae.models;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,10 +17,18 @@ import lombok.NoArgsConstructor;
 public class Course {
     @Id
     private String id;
+    
+    @NotBlank
     private String title;
+
+    @NotNull
     private int credits;
+
+    @NotNull
     private Section section;
+
+    @NotNull
     private int hours;
-    //private List<Student> students;
+    // private List<Student> students;
 
 }
--- src/main/java/webg6/pae/models/Student.java
+++ src/main/java/webg6/pae/models/Student.java
@@ -3,6 +3,8 @@ package webg6.pae.models;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,9 +17,17 @@ import lombok.NoArgsConstructor;
 public class Student {
     @Id
     private int id;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String fname;
+
+    @NotNull
     private Gender gender;
+
+    @NotNull
     private Section section;
     //private List<Course> courses;
 }
