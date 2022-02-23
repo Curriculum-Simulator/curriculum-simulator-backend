b8e9521d8c5ee74c56a391665920c3583f26c501
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-02-23 23:03:58 +0100

    Add CourseGraph class a course graph

---
+++ src/main/java/alahyaoui/curriculum/business/CourseGraph.java
@@ -0,0 +1,40 @@
+package webg6.pae.business;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class Graph {
+
+    private Set<CourseNode> nodes;// = new HashSet<>();
+
+    public Graph() {
+        nodes = new HashSet<>();
+    }
+
+    public void addNode(CourseNode node) {
+        nodes.add(node);
+    }
+
+    public void addNodes(List<CourseNode> nodes) {
+        for (CourseNode node : nodes) {
+            this.nodes.add(node);
+        }
+    }
+
+    public CourseNode search(String id) {
+        for (Iterator<CourseNode> it = nodes.iterator(); it.hasNext();) {
+            CourseNode node = it.next();
+            if ((id.equals(node.getId()))) {
+                return node;
+            }
+        }
+        return null;
+    }
+
+    public Set<CourseNode> getNodes() {
+        return nodes;
+    }
+
+}
