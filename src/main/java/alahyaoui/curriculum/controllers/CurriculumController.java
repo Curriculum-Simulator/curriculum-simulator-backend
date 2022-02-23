7c7bd50eff8d942572d83659f6b3bf8b17ca3227
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-02-23 23:05:14 +0100

    Fix package name

---
+++ src/main/java/alahyaoui/curriculum/controllers/CurriculumController.java
@@ -0,0 +1,11 @@
+package webg6.pae.controllers;
+
+import lombok.RequiredArgsConstructor;
+import webg6.pae.business.Curriculum;
+
+@RequiredArgsConstructor
+public class CurriculumController {
+
+    private final Curriculum curriculum;
+    
+}
