f26d24702b1e96ff3b5a068312f5909f6a91adb0
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-02-25 20:37:57 +0100

    Refactor Program class

---
+++ src/main/java/alahyaoui/curriculum/business/Program.java
@@ -0,0 +1,23 @@
+package alahyaoui.curriculum.business;
+
+import java.util.HashMap;
+import alahyaoui.curriculum.dto.ProgramCourseDto;
+import lombok.Value;
+
+@Value
+public class Program {
+
+    private final HashMap<String, ProgramCourseDto> studentCourses;
+
+    public Program() {
+        this.studentCourses = new HashMap<>();
+    }
+
+    public Program(HashMap studentCourses) {
+        this.studentCourses = studentCourses;
+    }
+
+    public ProgramCourseDto getStudentProgramCourse(String courseId) {
+        return studentCourses.get(courseId);
+    }
+}
