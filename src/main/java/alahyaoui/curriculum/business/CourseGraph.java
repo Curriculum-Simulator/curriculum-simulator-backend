da24e2a4dd90b517adef07b48b2ea001c8c5ccc0
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-02-26 23:04:56 +0100

    Refactor

--- src/main/java/alahyaoui/curriculum/business/CourseGraph.java
+++ src/main/java/alahyaoui/curriculum/business/CourseGraph.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class CourseGraph {
 
-    private Set<CourseNode> nodes;// = new HashSet<>();
+    private Set<CourseNode> nodes;
 
     public CourseGraph() {
         nodes = new HashSet<>();
--- src/main/java/alahyaoui/curriculum/business/Program.java
+++ src/main/java/alahyaoui/curriculum/business/Program.java
@@ -1,23 +1,23 @@
 package alahyaoui.curriculum.business;
 
 import java.util.HashMap;
-import alahyaoui.curriculum.dto.ProgramCourseDto;
+import alahyaoui.curriculum.dto.CourseStateDto;
 import lombok.Value;
 
 @Value
 public class Program {
 
-    private final HashMap<String, ProgramCourseDto> studentCourses;
+    private final HashMap<String, CourseStateDto> studentCourses;
 
     public Program() {
         this.studentCourses = new HashMap<>();
     }
 
-    public Program(HashMap<String, ProgramCourseDto> studentCourses) {
+    public Program(HashMap<String, CourseStateDto> studentCourses) {
         this.studentCourses = studentCourses;
     }
 
-    public ProgramCourseDto getStudentProgramCourse(String courseId) {
+    public CourseStateDto getStudentProgramCourse(String courseId) {
         return studentCourses.get(courseId);
     }
 }
