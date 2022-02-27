b71eb283505c5d36ff6fbe3724b545bad24e6139
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-02-27 19:56:50 +0100

    Rename studentCourses field to coursesToStates

--- src/main/java/alahyaoui/curriculum/business/Program.java
+++ src/main/java/alahyaoui/curriculum/business/Program.java
@@ -1,23 +1,34 @@
 package alahyaoui.curriculum.business;
 
 import java.util.HashMap;
+import java.util.List;
+
 import alahyaoui.curriculum.dto.CourseStateDto;
+import alahyaoui.curriculum.model.Course;
 import lombok.Value;
 
 @Value
 public class Program {
 
-    private final HashMap<String, CourseStateDto> studentCourses;
+    private final HashMap<String, CourseStateDto> coursesToStates;
 
     public Program() {
-        this.studentCourses = new HashMap<>();
+        this.coursesToStates = new HashMap<>();
     }
 
     public Program(HashMap<String, CourseStateDto> studentCourses) {
-        this.studentCourses = studentCourses;
+        this.coursesToStates = studentCourses;
+    }
+
+    public Program(List<Course> courses) {
+        this.coursesToStates = new HashMap<>();
+        for (var course : courses) {
+            String key = course.getId();
+            coursesToStates.put(key, new CourseStateDto());
+        }
     }
 
     public CourseStateDto getStudentProgramCourse(String courseId) {
-        return studentCourses.get(courseId);
+        return coursesToStates.get(courseId);
     }
 }
