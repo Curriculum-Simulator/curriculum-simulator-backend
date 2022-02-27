894af06e8bb921a01b8ee668ece19014ebc2ee1b
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-02-27 20:49:48 +0100

    Add new attribute to course

--- src/main/java/alahyaoui/curriculum/model/Course.java
+++ src/main/java/alahyaoui/curriculum/model/Course.java
@@ -2,9 +2,13 @@ package alahyaoui.curriculum.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+
+import org.hibernate.validator.constraints.Range;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,12 +19,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Course {
-    
+
+    private static final int MIN_QUARTER = 1;
+    private static final int MAX_QUARTER = 6;
+
     @Id
     private String id;
 
     @NotBlank(message = "Title is mandatory")
     private String title;
+
+    @Min(value = MIN_QUARTER, message = "Tickets must be greater than or equal to " + MIN_QUARTER)
+    @Max(value = MAX_QUARTER, message = "Block must be smaller than or equal to 3" + MAX_QUARTER)
+    private int quarter;
 
     @Positive(message = "Credits must be positive")
     private int credits;
