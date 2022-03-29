25c48e314de36ecc52550d5e1fec6aa307d1f343
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-03-29 19:40:08 +0200

    Add CourseStateDto documentation

--- src/main/java/alahyaoui/curriculum/dto/CourseStateDto.java
+++ src/main/java/alahyaoui/curriculum/dto/CourseStateDto.java
@@ -2,6 +2,10 @@ package alahyaoui.curriculum.dto;
 
 import lombok.Data;
 
+/**
+ * A course state contains a boolean value indicating whether a course has been passed or not, and a boolean
+ * value indicating whether a course is accessible or not
+ */
 @Data
 public class CourseStateDto {
 
