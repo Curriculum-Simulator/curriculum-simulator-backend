d24d92caad77e6b3d8114c41815c4efb157b8b27
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-03-29 19:10:32 +0200

    Add CourseNode Documentation

--- src/main/java/alahyaoui/curriculum/business/CourseNode.java
+++ src/main/java/alahyaoui/curriculum/business/CourseNode.java
@@ -9,23 +9,47 @@ import lombok.Value;
 public class CourseNode {
 
     private final String id;
+
+    // Creating a set of CourseNode objects that are the prerequisites for the current course.
     private final Set<CourseNode> prerequisites;
+
+    // Creating a set of CourseNode objects that are the corequisites for the current course.
     private final Set<CourseNode> corequisites;
 
+    /**
+     * This is the constructor of the CourseNode class. It initializes the id, the prerequisites and corequisites sets.
+     * @param id the CourseNode id.
+     */
     public CourseNode(String id){
         this.id = id;
         prerequisites = new HashSet<>();
         corequisites = new HashSet<>();
     }
 
+    /**
+     * Add a prerequisite to the course
+     * 
+     * @param prerequisite The prerequisite course.
+     */
     public void addPrerequisite(CourseNode prerequisite){
         prerequisites.add(prerequisite);
     }
 
+    /**
+     * Add a corequisite to the list of corequisites
+     * 
+     * @param corequisite the course that is a corequisite of the current course
+     */
     public void addCorequisite(CourseNode corequisite){
         corequisites.add(corequisite);
     }
 
+    /**
+     * Given a course ID, return the prerequisite node for that course
+     * 
+     * @param id The id of the prerequisite course.
+     * @return The CourseNode object that matches the id.
+     */
     public CourseNode searchPrerequisite(String id) {
         for (Iterator<CourseNode> it = prerequisites.iterator(); it.hasNext();) {
             CourseNode node = it.next();
@@ -36,6 +60,12 @@ public class CourseNode {
         return null;
     }
 
+    /**
+     * Search for a prerequisite course in the list of prerequisites
+     * 
+     * @param id The id of the prerequisite course.
+     * @return The CourseNode object that matches the id.
+     */
     public CourseNode searchCorequisite(String id) {
         for (Iterator<CourseNode> it = prerequisites.iterator(); it.hasNext();) {
             CourseNode node = it.next();
