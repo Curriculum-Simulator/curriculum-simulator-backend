3d92b580e7986ffc7109c532886fddc4f79b3558
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-04-07 00:39:16 +0200

    Add handling of not valid section

--- src/main/java/alahyaoui/curriculum/advice/SectionExceptionController.java
+++ src/main/java/alahyaoui/curriculum/advice/SectionExceptionController.java
@@ -7,14 +7,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import alahyaoui.curriculum.exception.SectionNotFoundException;
+import alahyaoui.curriculum.exception.SectionNotValidException;
 
 @ControllerAdvice
-public class SectionNotFoundAdvice {
+public class SectionExceptionController {
 
     @ResponseBody
     @ExceptionHandler(SectionNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String sectionNotFoundHandler(SectionNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(SectionNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    String sectionNotValidHandler(SectionNotValidException ex) {
         return ex.getMessage();
     }
 }
