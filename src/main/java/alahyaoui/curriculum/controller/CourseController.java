f5758e5f23d5b704f241ced4b9fa7c19bf9f864c
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-04-07 00:51:28 +0200

    Make controller methods to return only ResponseEntity

--- src/main/java/alahyaoui/curriculum/controller/CourseController.java
+++ src/main/java/alahyaoui/curriculum/controller/CourseController.java
@@ -2,7 +2,6 @@ package alahyaoui.curriculum.controller;
 
 import java.util.List;
 
-import org.apache.commons.lang3.EnumUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import alahyaoui.curriculum.exception.SectionNotFoundException;
 import alahyaoui.curriculum.model.Course;
 import alahyaoui.curriculum.model.Section;
 import alahyaoui.curriculum.service.CourseService;
