8fe96619d0db5bb2eabfd35b4eec28bc5270d60e
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-09 02:58:38 +0200

    Organize imports

--- src/main/java/alahyaoui/curriculum/controller/ProgramRestController.java
+++ src/main/java/alahyaoui/curriculum/controller/ProgramRestController.java
@@ -1,12 +1,11 @@
 package alahyaoui.curriculum.controller;
 
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
